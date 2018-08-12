b82ea6269131525e54ca9c46ef16f6d1f3b54e0f
bowen-xiao96 <39450130+bowen-xiao96@users.noreply.github.com>
2018-08-11 21:32:41 -0400

    extract and visualize score maps

---
+++ extract_attention_map.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.autograd as A
+
+from pay_attention import *
+
+
+def extract_attention_maps(model, x, size=(32, 32)):
+    feature_maps = list()
+    score_maps = list()
+
+    feature_maps = list()
+    for i, layer in enumerate(model.backbone):
+        x = layer(x)
+
+        # after relu layer
+        if i in model.attention_layers:
+            feature_maps.append(x)
+
+    # global feature
+    x = x.view(x.size(0), -1)
+    x = model.fc1(x)
+
+    features = list()
+    for i, feature_map in enumerate(feature_maps):
+        if len(model.attention[i]) == 2:
+            # project the global feature
+            new_x = model.attention[i][0](x)
+        else:
+            new_x = x
+
+        # attention score map (do 1x1 convolution on the addition of feature map and the global feature)
+        score = model.attention[i][-1](feature_map + new_x.view(new_x.size(0), -1, 1, 1))
+        old_shape = score.size()
+        score = F.softmax(
+            score.view(old_shape[0], -1), dim=1
+        ).view(old_shape)
+
+        # upsample the spatial map
+        score = F.upsample(score, size=size, mode='bilinear')
+        score_maps.append(score.data.cpu().numpy())
+
+    score_maps = np.concatenate(score_maps, axis=1)
+    return score_maps
+
+
+if __name__ == '__main__':
+    mean = np.array([0.49139968, 0.48215827, 0.44653124])
+    std = np.array([0.24703233, 0.24348505, 0.26158768])
+    normalize = transforms.Normalize(mean, std)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    size = 32
+    sample_count = 50
+    cifar10_dir = '/data2/bowenx/attention/cifar'
+
+    model_path = r'/data2/bowenx/attention/cifar/attention/best.pkl'
+    _, _, state_dict = torch.load(model_path)
+    model = AttentionNetwork(cfg, attention_layers, 10)
+    model.load_state_dict(state_dict)
+    model.eval()
+    model.cuda()
+    del state_dict
+
+    for flag in ('train', 'test'):
+        dataset = CIFAR10(
+            cifar10_dir,
+            train=True if flag == 'train' else False,
+        )
+
+        dataloader = DataLoader(
+            dataset,
+            batch_size=sample_count,
+            shuffle=True,
+            num_workers=0,
+            collate_fn=lambda batch: zip(*batch),
+            pin_memory=True,
+            drop_last=True
+        )
+
+        for x, _ in dataloader:
+            x_var = A.Variable(
+                torch.stack([transform(img) for img in x]).cuda()
+            )
+
+            score_maps = extract_attention_maps(model, x_var)
+            images = np.stack([np.array(img) for img in x])
+            np.savez(flag + '.npz', images=images, score_maps=score_maps)
+            break
---
+++ visualize_attention_maps.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from PIL import Image
+
+
+def get_jet():
+    colormap_int = np.zeros((256, 3), np.uint8)
+
+    for i in range(0, 256, 1):
+        colormap_int[i, 0] = np.uint8(np.round(cm.jet(i)[0] * 255.0))
+        colormap_int[i, 1] = np.uint8(np.round(cm.jet(i)[1] * 255.0))
+        colormap_int[i, 2] = np.uint8(np.round(cm.jet(i)[2] * 255.0))
+
+    return colormap_int
+
+
+def gray2color(gray_array, color_map):
+    rows, cols = gray_array.shape
+    color_array = np.zeros((rows, cols, 3), dtype=np.uint8)
+
+    for i in range(0, rows):
+        for j in range(0, cols):
+            color_array[i, j] = color_map[gray_array[i, j]]
+
+    color_image = Image.fromarray(color_array)
+    return color_image
+
+
+if __name__ == '__main__':
+    size = 32
+    padding = 10
+    color_map = get_jet()
+
+    for flag in ('train', 'test'):
+        data = np.load(flag + '.npz')
+        images, score_maps = data['images'], data['score_maps']
+
+        for i in range(images.shape[0]):
+            image = images[i]
+
+            output_img = np.zeros((size, size * 4 + padding * 3, 3), dtype=np.uint8)
+            output_img[:, :size, :] = image
+
+            for j in range(3):
+                output_img[:, (size + padding) * (j + 1):size * (j + 2) + padding * (j + 1), :] = \
+                    gray2color((255.0 * score_maps[i, j]).astype(np.uint8), color_map)
+
+            Image.fromarray(output_img, mode='RGB').save('%s_%d.png' % (flag, i))
