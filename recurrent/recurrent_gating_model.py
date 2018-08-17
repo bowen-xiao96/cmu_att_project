a5eefcb0f5b963a04bf64bebed7a1ee40c5af562
bowen-xiao96 <mike.xiao@pku.edu.cn>
2018-08-16 23:13:57 -0400

    Gated mixture

--- recurrent/recurrent_gating_model.py
+++ recurrent/recurrent_gating_model.py
@@ -1,3 +1,4 @@
+import os, sys
 import math
 import numpy as np
 
@@ -6,46 +7,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as A
 
+import Trainer
+from pay_attention import initialize_vgg, get_dataloader
+
 network_cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
 
 
 class RecurrentGatingModel(nn.Module):
-    def __init__(self, network_cfg, unroll_count, num_class, input_size=224, dropout=0.5):
+    def __init__(self, network_cfg, unroll_count, num_class, dropout=0.5):
         super(RecurrentGatingModel, self).__init__()
         self.unroll_count = unroll_count
 
         # backbone network
         self.backbone = nn.ModuleList()
         input_dim = 3
-        stride = 1
         for v in network_cfg:
             if v == 'M':
                 self.backbone.append(nn.MaxPool2d(kernel_size=2, stride=2))
-                stride *= 2
             else:
                 self.backbone.append(nn.Conv2d(input_dim, v, kernel_size=3, padding=1))
                 self.backbone.append(nn.ReLU(inplace=True))
                 input_dim = v
 
         # classifier
-        feature_map_size = int(math.floor(float(input_size) / stride))
         self.classifier = nn.Sequential(
-            nn.Linear(feature_map_size * feature_map_size * 512, 1024),
-            nn.ReLU(inplace=True),
             nn.Dropout(p=dropout),
-            nn.Linear(1024, num_class)
+            nn.Linear(512, num_class)
         )
 
         # recurrent connection
         # start at conv3_1, end at conv4_3
         self.start_idx = 10
         self.end_idx = 22
-        self.recurrent = nn.Sequential(
-            nn.Conv2d(512 + 128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
+
+        self.gating = nn.Sequential(
+            nn.Conv2d(512 + 128, 128, kernel_size=1),
+            nn.Sigmoid()
         )
+
+        self.projection = nn.Conv2d(512, 128, kernel_size=1)
 
     def forward(self, x):
         recurrent_buf = list()
@@ -63,9 +63,10 @@ class RecurrentGatingModel(nn.Module):
         for i in range(self.unroll_count):
             prev = recurrent_buf[-1]
             x = F.upsample(x, scale_factor=2, mode='bilinear')
-            gate = self.recurrent(torch.cat((prev, x), dim=1))
-            x = prev * gate
-            recurrent_buf.append(x)
+
+            # gated mixing
+            gate = self.gating(torch.cat((x, prev), dim=1))
+            x = gate * self.projection(x) + (1.0 - gate) * prev
 
             # remaining layers
             for i in range(self.start_idx, self.end_idx + 1):
@@ -75,11 +76,51 @@ class RecurrentGatingModel(nn.Module):
             x = self.backbone[i](x)
 
         x = x.view(x.size(0), -1)
-        print(x.shape)
+        # print(x.shape)
         return self.classifier(x)
 
 
 if __name__ == '__main__':
+    assert len(sys.argv) > 1
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+    TAG = sys.argv[1]
+
     model = RecurrentGatingModel(network_cfg, 5, 10)
-    input_sample = A.Variable(torch.zeros(1, 3, 224, 224))
-    print(model(input_sample).data)
+    initialize_vgg(model)
+    model = nn.DataParallel(model).cuda()
+
+    train_loader, test_loader = get_dataloader(
+        '/data2/bowenx/attention/pay_attention/cifar',
+        256,
+        4
+    )
+
+    criterion = nn.CrossEntropyLoss().cuda()
+    init_lr = 0.001
+
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=init_lr,
+        weight_decay=5e-4
+    )
+
+
+    def lr_sched(optimizer, epoch):
+        lr = init_lr * (0.5 ** (epoch // 30))
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+    Trainer.start(
+        model=model,
+        optimizer=optimizer,
+        train_dataloader=train_loader,
+        test_dataloader=test_loader,
+        criterion=criterion,
+        max_epoch=180,
+        lr_sched=lr_sched,
+        display_freq=50,
+        output_dir=TAG,
+        save_every=20,
+        max_keep=20
+    )
