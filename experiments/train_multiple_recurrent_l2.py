704bc0e80820df9e2f534c794616abd5d5223b9f
bowen-xiao96 <mike.xiao@pku.edu.cn>
2018-08-28 09:54:58 -0400

    multiple recurrent model on imagenet

---
+++ experiments/train_multiple_recurrent_l2.py
@@ -0,0 +1,159 @@
+import os, sys
+import numpy as np
+import torch.backends.cudnn as cudnn
+cudnn.benchmark = True
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+sys.path.insert(0, '/data2/bowenx/attention/pay_attention')
+
+from util import Trainer
+from util.model_tools import initialize_vgg
+from model.multiple_recurrent_l import *
+from dataset.imagenet.get_imagenet_dataset import get_dataloader
+
+#
+# parse params
+#
+assert len(sys.argv) > 3
+# gpu_id, unroll_count, tag, weight_file
+GPU_ID = int(sys.argv[1])
+if GPU_ID != -1:
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(GPU_ID)
+else:
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+unroll_count = int(sys.argv[2])
+TAG = sys.argv[3]
+
+if len(sys.argv) > 4:
+    weight_file = sys.argv[4]
+else:
+    weight_file = None
+
+print('Unrolling time step: %d' % unroll_count)
+
+#
+# create model
+#
+connections = (
+    (13, 8, 256, 128, 2),
+    (20, 15, 512, 256, 2),
+    # (27, 22, 512, 512, 2)
+)
+model = MultipleRecurrentModel(network_cfg, connections, unroll_count, 1000)
+initialize_vgg(model)
+
+if weight_file:
+    print('Loading weight file: ' + weight_file)
+    state_dict = torch.load(weight_file)
+    if isinstance(state_dict, tuple):
+        state_dict = state_dict[-1]
+    state_dict = {k.replace('features', 'backbone'): v for k, v in state_dict.items()}
+
+    print(state_dict.keys())
+    print(model.state_dict().keys())
+
+    model.load_state_dict(state_dict, strict=False)
+    del state_dict
+
+if GPU_ID == -1:
+    model = nn.DataParallel(model)
+model.cuda()
+
+#
+# load dataset
+#
+train_loader, test_loader = get_dataloader(
+    '/data2/simingy/data/Imagenet',
+    44,
+    8
+)
+
+#
+# prepare loss function
+#
+gamma = 0.5
+weights = list()
+for i in range(unroll_count):
+    if i == 0:
+        weights.append(gamma)
+    else:
+        weights.append(weights[-1] * gamma)
+
+# like (gamma**5, gamma**4, gamma**3, gamma**2, gamma)
+weights = torch.FloatTensor(list(reversed(weights)))
+# normalize to sum=1.0
+weights /= torch.sum(weights)
+print('Loss weights for different time steps:')
+print(weights)
+weights = A.Variable(weights.cuda(), requires_grad=False)
+
+
+def criterion(pred, y):
+    # batch_size * unroll_count * num_class
+    old_size = pred.size()
+    batch_size, time_step = old_size[:2]
+    # flatten into (-1, num_class)
+    new_size = torch.Size([batch_size * time_step] + list(old_size[2:]))
+    intermediate_pred = pred.view(new_size)
+
+    loss_1 = F.cross_entropy(
+        intermediate_pred,
+        y.repeat(time_step, 1).transpose(0, 1).contiguous().view(-1),
+        reduce=False
+    )
+    # average over batches
+    loss = torch.sum(loss_1 * weights.repeat(batch_size)) / batch_size
+    return loss
+
+
+#
+# prepare optimizer and lr scheduler
+#
+
+vgg_params = list(model.module.backbone.parameters()) + list(model.module.classifier.parameters())
+gating_params = list(model.module.gating.parameters())
+
+optimizer = optim.Adam([
+    {'params': vgg_params, 'lr': 1e-6, 'weight_decay': 1e-4},
+    {'params': gating_params, 'lr': 1e-5, 'weight_decay': 1e-4},
+])
+
+
+def call_back(epoch, step, locals_dict, globals_dict):
+    optimizer = globals_dict['optimizer_']
+
+    if epoch == 0:
+        if step >= 100:
+            lr = 1e-4
+        else:
+            return
+
+    elif epoch == 1:
+        lr = 1e-5
+
+    else:
+        lr = 1e-6
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+Trainer.start(
+    model=model,
+    optimizer=optimizer,
+    train_dataloader=train_loader,
+    test_dataloader=test_loader,
+    criterion=criterion,
+    max_epoch=3,
+    lr_sched=None,
+    call_back_=call_back,
+    display_freq=50,
+    output_dir=TAG,
+    save_every=1,
+    max_keep=50
+)
---
+++ model/multiple_recurrent_l.py
@@ -0,0 +1,207 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as A
+
+network_cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+
+class GatingModule(nn.Module):
+    def __init__(self, channel_low, channel_high, upsample, gating_kernel, reprojection_kernel, dropout=0.5):
+        super(GatingModule, self).__init__()
+
+        # use a upsampled version of high level feature map
+        # (can be either interpolation and deconvolution)
+        # to concatenate with the low level feature map and generate a gate
+        # use this gate to mix the low level feature map and reprojected high level feature map
+
+        if isinstance(upsample, int):
+            # upsample is an int specifying the scale_factor
+            self.upsample = nn.Sequential(
+                nn.Upsample(scale_factor=upsample, mode='bilinear'),
+                nn.Conv2d(channel_high, channel_low, kernel_size=1),
+                nn.ReLU(inplace=True)
+            )
+
+        else:
+            # upsample param will be a tuple specifying the params of the deconvolution layer
+            # kernel_size, stride, padding, output_padding
+            ks, s, p, op = upsample
+
+            # add a ReLU layer after each convolution layer
+            self.upsample = nn.Sequential(
+                nn.ConvTranspose2d(channel_high, channel_high, kernel_size=ks, stride=s, padding=p, output_padding=op),
+                nn.ReLU(inplace=True)
+            )
+
+        padding_k = gating_kernel // 2
+        self.gating = nn.Sequential(
+            nn.Conv2d(channel_low + channel_high, channel_low, kernel_size=gating_kernel, padding=padding_k),
+            nn.Sigmoid()
+        )
+
+        padding_p = reprojection_kernel // 2
+        self.reproject = nn.Sequential(
+            nn.Conv2d(channel_high, channel_low, kernel_size=reprojection_kernel, padding=padding_p),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout)
+        )
+
+    def forward(self, low, high):
+        upsampled_high = self.upsample(high)
+        reprojected_high = self.reproject(upsampled_high)
+
+        # concatenate along the channel and generate gating
+        gate = self.gating(torch.cat((upsampled_high, low), dim=1))
+        output = gate * reprojected_high + low
+        return output
+
+
+class MultipleRecurrentModel(nn.Module):
+    def __init__(self, network_cfg, connections, unroll_count, num_class, last_dim=7, dropout=0.5):
+        super(MultipleRecurrentModel, self).__init__()
+
+        # connections: (high_layer, low_layer, high_layer_dim, low_layer_dim, scale_factor)
+        # high_layer, low_layer: layer index
+        # scale_factor: 2**n
+        # attach_classifier: a boolean value indicating whether to attach a classifier at the low_layer
+
+        # ** data flows from high_layer to low_layer (recurrent) **
+        # ** currently, there should not be two connections pointing to a same low_layer **
+
+        self.unroll_count = unroll_count
+
+        # backbone network
+        self.backbone = nn.ModuleList()
+        input_dim = 3
+        for v in network_cfg:
+            if v == 'M':
+                self.backbone.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            else:
+                self.backbone.append(nn.Conv2d(input_dim, v, kernel_size=3, padding=1))
+                self.backbone.append(nn.ReLU(inplace=True))
+                input_dim = v
+
+        # we use fully connected layer as classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * last_dim * last_dim, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, num_class)
+        )
+
+        # set up recurrent connections
+        self.end_layers = set([c[1] for c in connections])
+        self.min_end_layer = min(self.end_layers)
+        self.layer_count = len(self.backbone)
+
+        self.gating = nn.ModuleList()
+        self.point_to = [list() for _ in self.backbone]
+
+        # sort connections according to low_layer
+        # since there should not be two connections pointing to a same low_layer
+        # the modules in classifiers and gating modules are also in this order (low_layer)
+        connections = sorted(connections, key=lambda c: c[1])
+
+        for high_layer, low_layer, high_layer_dim, low_layer_dim, scale_factor in connections:
+            # can modify the settings below
+            # kernel_size, stride, padding, output_padding
+            if scale_factor == 1:
+                gating = GatingModule(low_layer_dim, high_layer_dim, (3, 1, 1, 0), 3, 1, dropout=0.0)
+            elif scale_factor == 2:
+                gating = GatingModule(low_layer_dim, high_layer_dim, (3, 2, 1, 1), 3, 1, dropout=0.0)
+            else:
+                # TODO: long connection across two blocks
+                raise NotImplementedError
+
+            self.gating.append(gating)
+            self.point_to[high_layer].append(low_layer)
+
+    def forward(self, x):
+        # each unrolling step, the model makes prediction at the final classifier
+
+        # the input to each layer (not output)
+        buf = [list() for _ in self.backbone]
+
+        # the intermediate predictions of the network
+        # the output should be a batch_size * unroll_count * num_class Tensor
+        intermediate_pred = list()
+
+        # layers before recurrent start
+        for i in range(self.min_end_layer):
+            # do not actually include the starting layer
+            x = self.backbone[i](x)
+
+        buf[self.min_end_layer].append(x)
+
+        # do recurrent stuff
+        for i in range(self.unroll_count):
+            if i == 0:
+                # the first unrolling step
+                # just straightforward forward computation
+                for j in range(self.min_end_layer, self.layer_count):
+                    x = self.backbone[j](x)
+
+                    # put it into the buffer if there is feedback connection
+                    for point_to in self.point_to[j]:
+                        buf[point_to].append(x)
+
+                # make final predictions
+                x = x.view(x.size(0), -1)
+                pred = self.classifier(x)
+                intermediate_pred.append(pred)
+
+            else:
+                # we need to apply recurrent to the input of each recurrent block
+                # and make intermediate predictions using the prediction module
+
+                # record which gating_models to use
+                pos = 0
+
+                low, high = buf[self.min_end_layer][-2:]
+                x = self.gating[pos](low, high)
+                buf[self.min_end_layer].append(x)
+                pos += 1
+
+                for j in range(self.min_end_layer, self.layer_count):
+                    # forward computation of this layer
+                    if j in self.end_layers and j != self.min_end_layer:
+                        low, high = x, buf[j][-1]
+                        x = self.gating[pos](low, high)
+                        pos += 1
+
+                    x = self.backbone[j](x)
+
+                    # put it into the buffer if there is feedback connection
+                    for point_to in self.point_to[j]:
+                        buf[point_to].append(x)
+
+                # make final predictions
+                x = x.view(x.size(0), -1)
+                pred = self.classifier(x)
+                intermediate_pred.append(pred)
+
+        # collect predictions
+        # batch_size * unroll_count * num_class
+        intermediate_pred = torch.stack(intermediate_pred, dim=1)
+        return intermediate_pred
+
+
+if __name__ == '__main__':
+    # high_layer, low_layer, high_layer_dim, low_layer_dim, scale_factor
+    connections = (
+        (6, 3, 128, 64, 2),
+        (13, 8, 256, 128, 2),
+        (20, 15, 512, 256, 2),
+        (27, 22, 512, 512, 2)
+    )
+
+    # works for imagenet images
+    model = MultipleRecurrentModel(network_cfg, connections, 5, 1000)
+    print(model)
+    model_input = A.Variable(torch.zeros(5, 3, 224, 224))
+    model_output = model(model_input)
+    print(model_output.size())
